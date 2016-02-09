fee92e7d37c7448168d971c98074ad73503d62e9
titkonen <toni.itkonen@icloud.com>
2016-02-09 16:09:28 +0200

    Version about no error. TableList UI is empty from the sample material.

---
+++ Rate-my-wine/MealTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  MealTableViewCell.swift
+//  Rate-my-wine
+//
+//  Created by Toni Itkonen on 30/01/16.
+//  Copyright © 2016 Toni Itkonen. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+    
+    // MARK: Properties
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
